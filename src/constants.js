97033378cc178a4c3b80d50f1f8d9679f0f08a53
Mariana Trujillo Islas <marianatrujillo27@hotmail.com>
2024-04-02 12:12:09 -0600

    feat(): index design

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const comment = [
+    {
+      opinion:
+        "¡Increíble herramienta! Antes pasaba horas llenando formularios manualmente para el traslado de ganado, pero esta página simplifica todo el proceso.",
+    },
+    {
+      opinion:
+        "Con esta página, ya no hay margen para errores humanos en los formularios de tránsito de ganado. Todos los campos están claramente definidos y validados, lo que garantiza la precisión de los datos. ",
+    },
+    {
+      opinion:
+        "La interfaz de usuario de esta página es muy intuitiva y fácil de usar. Incluso para alguien sin experiencia técnica, resulta sencillo completar los formularios de tránsito de ganado.",
+    },
+    {
+      opinion:
+        "El uso de esta página para generar formularios de tránsito de ganado ha reducido significativamente nuestros costos administrativos.",
+    },
+    {
+      opinion:
+        "Esta página administrativa ganadera asegura que cumplamos con todas las regulaciones y requisitos legales en cuanto al traslado de ganado.",
+    },
+  ];
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,164 +1,213 @@
 import withAuth from "@/components/Authenticated";
-import CustomButton from "@/components/CustomButton";
-import CustomCharacteristics from "@/components/CustomCharacteristics";
-import CustomImageParallax from "@/components/CustomImageParallax";
+import CustomCategories from "@/components/CustomCategories";
+import CustomComment from "@/components/CustomComment";
 import {
-  CharacteristicsStyled,
-  DescriptionContainer,
+  AboutUs,
+  AboutUsContainer,
+  Background,
+  ButtonStyled,
+  ButtonText,
+  ButtonsContainer,
+  CenterContainer,
+  CharacteristicsContainer,
+  Container,
+  GridContainer,
+  GridContainerAboutUs,
+  GridContainerCharacteristics,
   GridImage,
-  GridText,
-  ParallaxContent,
-  SpanStyled,
-  TextStyled,
-  VideoContainer,
-  VideoText,
+  GridImageAboutUs,
+  GridWelcome,
+  Icon,
+  ImgStyled,
+  ImgStyledAboutUs,
+  InformationContainer,
+  TextLeft,
+  TextRight,
+  Title,
+  Welcome,
+  WhoWeAre,
 } from "@/styles/Index.style";
-import { GridContainer } from "@/styles/Login.style";
+import { comment } from "/src/constants";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { Skeleton } from "@mui/material";
 import { useEffect, useState } from "react";
 
 const Home = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const loading = () => {
-    return (
-      <div>
-        <div>
-          <Skeleton height={650} variant="section" animation="wave" />
-        </div>
-        <TextStyled>
-          <Skeleton height={50} width={700} animation="wave" />
-        </TextStyled>
-        <GridContainer>
-          <Skeleton height={400} variant="section" animation="wave" />
-        </GridContainer>
-        <div>
-          <SpanStyled>
-            <Skeleton height={50} width={700} animation="wave" />
-          </SpanStyled>
-          <CharacteristicsStyled>
-            <Skeleton
-              height={350}
-              width={400}
-              variant="section"
-              animation="wave"
-            />
-            <Skeleton
-              height={350}
-              width={400}
-              variant="section"
-              animation="wave"
-            />
-            <Skeleton
-              height={350}
-              width={400}
-              variant="section"
-              animation="wave"
-            />
-          </CharacteristicsStyled>
-        </div>
-        <div style={{ marginBottom: "32px" }}>
-          <Skeleton height={200} variant="section" animation="wave" />
-        </div>
-        <GridContainer style={{ gap: "8px" }}>
-          <Skeleton height={500} variant="section" animation="wave" />
-          <Skeleton height={500} variant="section" animation="wave" />
-        </GridContainer>
-      </div>
-    );
-  };
+  const [currentCommentIndex, setCurrentCommentIndex] = useState(0);
 
   useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
+    const interval = setInterval(() => {
+      setCurrentCommentIndex((prevIndex) => (prevIndex + 1) % comment.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
   }, []);
+
+  // const [isLoading, setIsLoading] = useState(true);
+  // const loading = () => {
+  //   return (
+  //     <div>
+  //       <div>
+  //         <Skeleton height={650} variant="section" animation="wave" />
+  //       </div>
+  //       <TextStyled>
+  //         <Skeleton height={50} width={700} animation="wave" />
+  //       </TextStyled>
+  //       <GridContainer>
+  //         <Skeleton height={400} variant="section" animation="wave" />
+  //       </GridContainer>
+  //       <div>
+  //         <SpanStyled>
+  //           <Skeleton height={50} width={700} animation="wave" />
+  //         </SpanStyled>
+  //         <CharacteristicsStyled>
+  //           <Skeleton
+  //             height={350}
+  //             width={400}
+  //             variant="section"
+  //             animation="wave"
+  //           />
+  //           <Skeleton
+  //             height={350}
+  //             width={400}
+  //             variant="section"
+  //             animation="wave"
+  //           />
+  //           <Skeleton
+  //             height={350}
+  //             width={400}
+  //             variant="section"
+  //             animation="wave"
+  //           />
+  //         </CharacteristicsStyled>
+  //       </div>
+  //       <div style={{ marginBottom: "32px" }}>
+  //         <Skeleton height={200} variant="section" animation="wave" />
+  //       </div>
+  //       <GridContainer style={{ gap: "8px" }}>
+  //         <Skeleton height={500} variant="section" animation="wave" />
+  //         <Skeleton height={500} variant="section" animation="wave" />
+  //       </GridContainer>
+  //     </div>
+  //   );
+  // };
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setIsLoading(false);
+  //   }, 2000);
+  // }, []);
 
   return (
     <>
-      {isLoading ? (
-        loading()
-      ) : (
-        <div>
-          <div>
-            <CustomImageParallax
-              imageUrl="/img/index.png"
-              title="Agro en un solo lugar"
-            >
-              <ParallaxContent>
-                <span>
-                  Un punto de encuentro virtual para el mundo agropecuario,
-                  donde la venta de ganado es más que un negocio.
-                </span>
-                <CustomButton buttonText="Comenzar ahora" customIndexDesign />
-              </ParallaxContent>
-            </CustomImageParallax>
-          </div>
-          <TextStyled>
+      <GridContainer>
+        <GridWelcome>
+          <Welcome>
             <h1>
-              Formularios intuitivos para tu negocio ganadero.
-              <span>&#160;</span>
+              Gestiona tu <span>ganado</span> con un toque de innovación
             </h1>
-          </TextStyled>
-          <GridContainer>
-            <GridText>
-              <span>
-                Nuestra plataforma simplifica por completo el proceso de guías
-                de tránsito, desde el llenado hasta la entrega, garantizando que
-                su ganado llegue a su destino con la documentación correcta y
-                completa.
-              </span>
-            </GridText>
-            <GridImage>
-              <img src="/img/ImgInicio2.png" className="img1" />
-              <img src="/img/index3.jpg" className="img2" />
-            </GridImage>
-          </GridContainer>
-          <div>
-            <SpanStyled>¡Aquí podrás encontrar!</SpanStyled>
-            <CharacteristicsStyled>
-              <CustomCharacteristics
-                img="/img/laptop.png"
-                title="Facilidad de uso"
-                text="Crea formularios de tránsito de ganado de manera sencilla y sin complicaciones en solo unos pocos clics."
-              />
-              <CustomCharacteristics
-                img="/img/checklist.png"
-                title="Descarga tus guías"
-                text="Genera formularios de tránsito de ganado personalizados y descárgalos en PDF o Excel para un acceso conveniente."
-              />
-              <CustomCharacteristics
-                img="/img/book.png"
-                title="Acceso centralizado de guías"
-                text="Accede y visualiza guías de tránsito de ganado en un solo lugar, para un control completo de tu actividad ganadera."
-              />
-            </CharacteristicsStyled>
-          </div>
-          <DescriptionContainer>
-            <span className="title">¿A qué nos dedicamos?</span>
-            <span className="children">
-              En GanadoLink, simplificamos el proceso de guías de tránsito de
-              ganado con nuestros formularios innovadores. Facilitamos la
-              gestión de la documentación para productores y transportistas.
+            <p>
+              Simplifica la gestión, venta y transporte de ganado, uniendo a
+              ganaderos y compradores en un solo lugar.
+            </p>
+            <ButtonsContainer>
+              <ButtonStyled>
+                Comenzar a usar <Icon icon={faChevronRight} />
+              </ButtonStyled>
+              <ButtonText>Learn more</ButtonText>
+            </ButtonsContainer>
+          </Welcome>
+        </GridWelcome>
+        <GridImage>
+          <Background />
+          <ImgStyled src="/img/ganado.png" />
+        </GridImage>
+      </GridContainer>
+      <CharacteristicsContainer>
+        <Title>
+          <span>
+            ¡Tu <span className="color">administración</span> simplificada, en
+            un solo lugar!
+          </span>
+        </Title>
+        <GridContainerCharacteristics>
+          <CustomCategories
+            img="/img/caracteristica1.png"
+            title="Acceso centralizado de guías"
+            text="Accede y visualiza tus guías de tránsito de ganado de manera organizada y  sencilla en un solo lugar, para un control total de tu actividad  ganadera."
+          />
+          <CustomCategories
+            img="/img/caracteristica2.png"
+            title="Facilidad de uso"
+            text="Crea formularios de tránsito de ganado de manera sencilla y sin complicaciones en solo unos pocos clics."
+          />
+          <CustomCategories
+            img="/img/caracteristica3.png"
+            title="Seguridad de datos"
+            text="Tu información ganadera está protegida con los más altos estándares de  seguridad para garantizar la confidencialidad y la integridad de tus  registros."
+          />
+          <CustomCategories
+            img="/img/caracteristica4.png"
+            title="Descarga tus guías"
+            text="Crea y descarga fácilmente formularios personalizados de tránsito de  ganado en PDF o Excel para un acceso y almacenamiento más conveniente."
+          />
+        </GridContainerCharacteristics>
+      </CharacteristicsContainer>
+      <Container>
+        <WhoWeAre>
+          <span>Descubre qué nos hace únicos</span>
+        </WhoWeAre>
+        <GridContainerAboutUs>
+          <AboutUsContainer>
+            <span>
+              Optimizamos el proceso de guías de tránsito de ganado con nuestros
+              formularios innovadores, simplificando la gestión de documentación
+              tanto para productores como para transportistas.
             </span>
-          </DescriptionContainer>
-          <GridContainer>
-            <VideoText>
-              <span className="title">Lo que piensan nuestros clientes</span>
-              <span className="icon">"</span>
-              <span className="comment">
-                Una herramienta fantástica para generar formularios de tránsito
-                de ganado, fácil de usar y muy práctica. ¡Realmente útil para
-                mantener registros organizados!
+            <span>
+              Además, nuestra plataforma ofrece una experiencia intuitiva y
+              eficiente, asegurando que tanto productores como transportistas
+              puedan cumplir con los requisitos de documentación de manera
+              rápida y sin complicaciones.
+            </span>
+          </AboutUsContainer>
+          <CenterContainer>
+            <ImgStyledAboutUs src="/img/aboutus.jpg" alt="aboutus" />
+          </CenterContainer>
+        </GridContainerAboutUs>
+      </Container>
+      <Container>
+        <GridContainerAboutUs>
+          <CenterContainer>
+            <video
+              width="580px"
+              height="430px"
+              autoPlay
+              muted
+              loop
+              style={{ objectFit: "cover", borderRadius: "15px" }}
+            >
+              <source src="/img/video.mp4" type="video/mp4" />
+            </video>
+          </CenterContainer>
+          <CenterContainer>
+            <TextRight>
+              <span>
+                Lo que nuestros <span className="color">usuarios</span> piensan
+                de nosotros
               </span>
-            </VideoText>
-            <VideoContainer>
-              <video src="/img/cows.mp4" autoPlay muted className="video" />
-            </VideoContainer>
-          </GridContainer>
-        </div>
-      )}
+            </TextRight>
+            <CustomComment
+              key={currentCommentIndex}
+              opinion={comment[currentCommentIndex].opinion}
+              currentIndex={currentCommentIndex}
+              totalComments={comment.length}
+            />
+          </CenterContainer>
+        </GridContainerAboutUs>
+      </Container>
     </>
   );
-}
+};
 
 export default withAuth(Home);
